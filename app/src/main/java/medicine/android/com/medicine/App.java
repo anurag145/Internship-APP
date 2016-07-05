c7af0a0e434c007c935c1288e38a109b44d4efd4
Anurag Shrivastava <Anurag Shrivastava>
2016-07-06 01:40:41 +0530

    upgraded login system

--- app/src/main/java/medicine/android/com/medicine/App.java
+++ app/src/main/java/medicine/android/com/medicine/App.java
@@ -1,8 +1,15 @@
 package medicine.android.com.medicine;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.SystemClock;
+import android.util.Base64;
+import android.util.Log;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,5 +22,23 @@ public class App extends Application {
 
 
         SystemClock.sleep(TimeUnit.SECONDS.toMillis(1));
+        printHashKey();
+    }
+    public void printHashKey(){
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "medicine.android.com.medicine",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 }
--- app/src/main/java/medicine/android/com/medicine/Login.java
+++ app/src/main/java/medicine/android/com/medicine/Login.java
@@ -1,58 +1,29 @@
 package medicine.android.com.medicine;
 
+
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
 import android.widget.Button;
 import android.widget.ImageView;
-import java.net.MalformedURLException;
-import android.os.AsyncTask;
-import android.widget.Toast;
-
-import com.android.volley.toolbox.ImageLoader;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
-import com.microsoft.windowsazure.mobileservices.MobileServiceList;
-import com.microsoft.windowsazure.mobileservices.http.NextServiceFilterCallback;
-import com.microsoft.windowsazure.mobileservices.http.ServiceFilter;
-import com.microsoft.windowsazure.mobileservices.http.ServiceFilterRequest;
-import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
-import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
-
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
-import com.microsoft.windowsazure.mobileservices.*;
-import org.w3c.dom.Text;
 
 
 
-public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+public class Login extends AppCompatActivity  {
     CarouselView carouselView;
     int[] sampleImages = {R.mipmap.ic_launcher,R.mipmap.l,R.mipmap.m,R.mipmap.n};
 
-    MobileServiceClient mClient;
-    //Signin button
+
     private Button signInButton;
-
+private Button signInButton2;
     //Signing Options
-    private GoogleSignInOptions gso;
 
-    //google api client
-    private GoogleApiClient mGoogleApiClient;
 
-    //Signin constant to check the activity result
-    private int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,15 +37,23 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         carouselView.setImageListener(imageListener);
 
         //Initializing signinbutton
-        signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton = (Button) findViewById(R.id.sign_in_button_facebook);
+        signInButton2=(Button)findViewById(R.id.sign_in_button_google);
+
+        signInButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
         //Setting onclick listener to signing button
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent q = new Intent(Login.this,GoogleLogin.class);
-                startActivity(q);
+                Intent intent =new Intent( Login.this,FacebookLogin.class);
+                startActivity(intent);
             }
         });
 
@@ -98,8 +77,5 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
     };
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-    }
 }

d6a7b40e51e1d3f25807d2f2ad95f3cb247851f9
Anurag Shrivastava <Anurag Shrivastava>
2016-07-07 00:28:28 +0530

    azure login system

--- app/src/main/java/medicine/android/com/medicine/App.java
+++ app/src/main/java/medicine/android/com/medicine/App.java
@@ -1,16 +1,7 @@
 package medicine.android.com.medicine;
 
 import android.app.Application;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.os.SystemClock;
-import android.util.Base64;
-import android.util.Log;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Anurag on 6/24/2016.
@@ -21,24 +12,8 @@ public class App extends Application {
         super.onCreate();
 
 
-        SystemClock.sleep(TimeUnit.SECONDS.toMillis(1));
-        printHashKey();
-    }
-    public void printHashKey(){
-        // Add code to print out the key hash
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "medicine.android.com.medicine",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
 
-        } catch (NoSuchAlgorithmException e) {
 
-        }
     }
+
 }
--- app/src/main/java/medicine/android/com/medicine/Login.java
+++ app/src/main/java/medicine/android/com/medicine/Login.java
@@ -1,13 +1,31 @@
 package medicine.android.com.medicine;
 
 
+import android.app.Dialog;
 import android.content.Intent;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import com.microsoft.windowsazure.mobileservices.authentication.MobileServiceAuthenticationProvider;
+import com.microsoft.windowsazure.mobileservices.authentication.MobileServiceUser;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.microsoft.windowsazure.mobileservices.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
@@ -17,10 +35,13 @@ import com.synnapps.carouselview.ImageListener;
 public class Login extends AppCompatActivity  {
     CarouselView carouselView;
     int[] sampleImages = {R.mipmap.ic_launcher,R.mipmap.l,R.mipmap.m,R.mipmap.n};
-
-
+    private TextView textview;
+    private MobileServiceClient mClient;
     private Button signInButton;
 private Button signInButton2;
+    public static final String SHAREDPREFFILE = "temp";
+    public static final String USERIDPREF = "uid";
+    public static final String TOKENPREF = "tkn";
     //Signing Options
 
 
@@ -30,13 +51,32 @@ private Button signInButton2;
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+
+        try {
+            mClient = new MobileServiceClient(
+                    "https://twokilo.azure-mobile.net/",
+                    "bprbEdBfMdPozKNKnLcUQKPEgUKgof22",
+                    this
+
+            );
+
+            if (loadUserTokenCache(mClient))
+            {
+                Intent intent = new Intent(Login.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }catch (Exception e)
+        {
+            Log.e("hello",e.toString());
+        }
         carouselView = (CarouselView) findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
 
 
 
         carouselView.setImageListener(imageListener);
-
+         textview=(TextView)findViewById(R.id.textView6);
         //Initializing signinbutton
         signInButton = (Button) findViewById(R.id.sign_in_button_facebook);
         signInButton2=(Button)findViewById(R.id.sign_in_button_google);
@@ -44,8 +84,23 @@ private Button signInButton2;
         signInButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent =new Intent(Login.this,GoogleLogin.class);
-                startActivity(intent);
+                ListenableFuture<MobileServiceUser> mLogin = mClient.login(MobileServiceAuthenticationProvider.Google);
+
+                Futures.addCallback(mLogin, new FutureCallback<MobileServiceUser>() {
+                    @Override
+                    public void onFailure(Throwable exc) {
+                        Log.e("hello",exc.toString());
+                    }
+                    @Override
+                    public void onSuccess(MobileServiceUser user) {
+
+                        cacheUserToken(mClient.getCurrentUser());
+                        Intent intent = new Intent(Login.this,MainActivity.class);
+
+                        startActivity(intent);
+                        finish();
+                    }
+                });
             }
         });
 
@@ -54,9 +109,22 @@ private Button signInButton2;
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ListenableFuture<MobileServiceUser> mLogin = mClient.login(MobileServiceAuthenticationProvider.Facebook);
 
-                Intent intent =new Intent( Login.this,FacebookLogin.class);
-                startActivity(intent);
+                Futures.addCallback(mLogin, new FutureCallback<MobileServiceUser>() {
+                    @Override
+                    public void onFailure(Throwable exc) {
+                        Log.e("hello",exc.toString());
+                    }
+                    @Override
+                    public void onSuccess(MobileServiceUser user) {
+
+                        cacheUserToken(mClient.getCurrentUser());
+                        Intent intent = new Intent(Login.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
             }
         });
 
@@ -79,6 +147,29 @@ private Button signInButton2;
 
 
     };
+    private void cacheUserToken(MobileServiceUser user)
+    {
+        SharedPreferences prefs = getSharedPreferences(SHAREDPREFFILE, Context.MODE_PRIVATE);
+        Editor editor = prefs.edit();
+        editor.putString(USERIDPREF, user.getUserId());
+        editor.putString(TOKENPREF, user.getAuthenticationToken());
+        editor.commit();
+    }
+    private boolean loadUserTokenCache(MobileServiceClient client)
+    {
+        SharedPreferences prefs = getSharedPreferences(SHAREDPREFFILE, Context.MODE_PRIVATE);
+        String userId = prefs.getString(USERIDPREF, null);
+        if (userId == null)
+            return false;
+        String token = prefs.getString(TOKENPREF, null);
+        if (token == null)
+            return false;
 
+        MobileServiceUser user = new MobileServiceUser(userId);
+        user.setAuthenticationToken(token);
+        client.setCurrentUser(user);
+
+        return true;
+    }
 
 }
